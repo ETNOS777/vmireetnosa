9e323e87cc8287644f182cd518e62f73e5ba2e8e
ETNOS777 <yellowsubmarine228@gmail.com>
2024-05-15 14:29:37 +0300

    Добавление контента

---
+++ js/main.js
@@ -0,0 +1,19 @@
+( function($) {
+  'use strict';
+
+
+
+  	$('.main-slider').slick({
+		  dots: true,
+		  infinite: true,
+		  speed: 500,
+		  fade: true,
+		  autoplay: true,
+  		autoplaySpeed: 4000,
+		  cssEase: 'linear'
+		});
+    
+
+
+
+})(jQuery);
